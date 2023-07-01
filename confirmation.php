235bdf865fce2f30abb0b3e87c2c7b207e208bba
Trevor Ransom <transom1@outlook.com>
2023-07-01 13:57:42 -0700

    Add files via upload

---
+++ confirmation.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Travel Confirmation</title>
+  <link rel="stylesheet" href="confirmation.css?v=<?php echo time(); ?>">
+</head>
+<body>
+  <h1>travel confirmation</h1>
+  <div class="confirmation">
+    <?php include 'confirmation_fill.php';?>
+</div>
+</body>
+<div class="sidebar_pic1">
+  <a href="http://jettrav.com"><img src="boat.jpg"></a>
+</div>
+<div class="sidebar_pic2">
+  <a href="http://jettrav.com"><img src="beach_rock.jpg"></a>
+</div>
+<div class="sidebar_pic3">
+  <a href="http://jettrav.com"><img src="hawaii.jpg"></a>
+</div>
+</html>
