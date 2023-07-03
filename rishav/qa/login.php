a6ad0f939375e87cd347e8bae7d2c8773ea424d0
rishav <79098022+hellorishav@users.noreply.github.com>
2023-07-03 18:14:55 -0500

    Update login.php

--- rishav/qa/login.php
+++ rishav/qa/login.php
@@ -17,7 +17,7 @@
         }
 
         .container {
-            max-width: 800px;
+            max-width: 400px;
             width: 100%;
             padding: 40px;
             background-color: #fff;
@@ -82,11 +82,39 @@
             vertical-align: middle;
         }
 
-        .tab {
-            display: none;
+        .tab-links {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            display: flex;
+            justify-content: space-between;
         }
 
-        .active {
+        .tab-link {
+            flex-grow: 1;
+            padding: 10px;
+            background-color: #eee;
+            border-radius: 4px 4px 0 0;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        .tab-link:hover {
+            background-color: #ddd;
+        }
+
+        .tab-link.active {
+            background-color: #fff;
+        }
+
+        .tab-content {
+            display: none;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 0 4px 4px 4px;
+        }
+
+        .tab-content.active {
             display: block;
         }
 
@@ -99,50 +127,39 @@
             color: #009900;
             margin-bottom: 10px;
         }
-
-        .welcome-message {
-            text-align: left;
-            margin-bottom: 20px;
-        }
-
-        .logout-button {
-            float: right;
-            padding: 5px 10px;
-            background-color: #ff0000;
-            color: #fff;
-            border: none;
-            border-radius: 4px;
-            font-size: 14px;
-            cursor: pointer;
-        }
-
-        .dashboard {
-            text-align: left;
-        }
-
-        .search-box {
-            margin-bottom: 10px;
-        }
-
-        .leads-table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        .leads-table th,
-        .leads-table td {
-            padding: 8px;
-            border: 1px solid #ddd;
-        }
     </style>
     <script>
-        function switchTab(tabName) {
-            var tabs = document.getElementsByClassName('tab');
-            for (var i = 0; i < tabs.length; i++) {
-                tabs[i].style.display = 'none';
-            }
+        document.addEventListener('DOMContentLoaded', function() {
+            // Set the login tab as active by default
+            document.querySelector('.tab-link[data-tab="login-tab"]').classList.add('active');
+            document.getElementById('login-tab').classList.add('active');
 
-            document.getElementById(tabName).style.display = 'block';
+            // Add event listener to tab links for tab switching
+            var tabLinks = document.querySelectorAll('.tab-link');
+            tabLinks.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    var tabName = this.dataset.tab;
+                    switchTab(tabName);
+                });
+            });
+        });
+
+        function switchTab(tabName) {
+            var tabLinks = document.querySelectorAll('.tab-link');
+            var tabContents = document.querySelectorAll('.tab-content');
+
+            // Remove active class from all tab links and contents
+            tabLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+
+            tabContents.forEach(function(content) {
+                content.classList.remove('active');
+            });
+
+            // Add active class to the clicked tab link and corresponding content
+            document.querySelector('.tab-link[data-tab="' + tabName + '"]').classList.add('active');
+            document.getElementById(tabName).classList.add('active');
         }
     </script>
 </head>
@@ -151,13 +168,13 @@
         <h1>Authentication System</h1>
 
         <ul class="tab-links">
-            <li><a href="#" onclick="switchTab('login-tab')">Login</a></li>
+            <li><a class="tab-link" href="#" data-tab="create-account-tab">Create Account</a></li>
+            <li><a class="tab-link" href="#" data-tab="login-tab">Login</a></li>
         </ul>
 
-        <div id="login-tab" class="tab active">
+        <div id="create-account-tab" class="tab-content">
+            <h2>Create Account</h2>
             <?php
-            session_start();
-
             $servername = "localhost";
             $username = "u947421468_jettrav";
             $password = "Jettrav@capstone1";
@@ -171,6 +188,51 @@
                 die("Connection failed: " . $conn->connect_error);
             }
 
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create-account'])) {
+                $name = $_POST['name'];
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                // Check if the username already exists
+                $checkQuery = "SELECT * FROM credentials WHERE username = '$username'";
+                $checkResult = $conn->query($checkQuery);
+
+                if ($checkResult && $checkResult->num_rows > 0) {
+                    echo '<div class="error-message">Username already exists. Please choose a different username.</div>';
+                } else {
+                    // Hash the password
+                    $hashedPassword = hash('sha256', $password);
+
+                    // Insert the new account into the database
+                    $insertQuery = "INSERT INTO credentials (name, username, password, role) VALUES ('$name', '$username', '$hashedPassword', 'user')";
+                    if ($conn->query($insertQuery) === TRUE) {
+                        echo '<div class="success-message">Account created successfully.</div>';
+                    } else {
+                        echo '<div class="error-message">Error creating account: ' . $conn->error . '</div>';
+                    }
+                }
+            }
+            ?>
+            <form method="post" action="">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name" required>
+                </div>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <input type="submit" name="create-account" value="Create Account">
+            </form>
+        </div>
+
+        <div id="login-tab" class="tab-content">
+            <h2>Login</h2>
+            <?php
             if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
                 $username = $_POST['username'];
                 $password = $_POST['password'];
@@ -183,130 +245,23 @@
                 $loginResult = $conn->query($loginQuery);
 
                 if ($loginResult && $loginResult->num_rows > 0) {
-                    // Login successful, store user information in session
-                    $_SESSION['username'] = $username;
-                    $_SESSION['name'] = $loginResult->fetch_assoc()['name'];
+                    echo '<div class="success-message">Login successful.</div>';
                 } else {
                     echo '<div class="error-message">Invalid username or password.</div>';
                 }
             }
-
-            // Check if user is logged in
-            if (isset($_SESSION['username'])) {
-                $name = $_SESSION['name'];
-                echo '<div class="welcome-message">Welcome, ' . $name . '!</div>';
-                echo '<form method="post" action="">';
-                echo '<input class="logout-button" type="submit" name="logout" value="Logout">';
-                echo '</form>';
-
-                // Logout functionality
-                if (isset($_POST['logout'])) {
-                    session_unset();
-                    session_destroy();
-                    header("Location: login.php");
-                    exit();
-                }
-
-                // Fetch leads from the database
-                $leadsQuery = "SELECT * FROM leads";
-                $leadsResult = $conn->query($leadsQuery);
-
-                if ($leadsResult && $leadsResult->num_rows > 0) {
-                    echo '<div class="dashboard">';
-                    echo '<h2>Leads Dashboard</h2>';
-                    echo '<div class="search-box">';
-                    echo '<label for="search-input">Search:</label>';
-                    echo '<input type="text" id="search-input" onkeyup="searchLeads()" placeholder="Enter search criteria">';
-                    echo '</div>';
-                    echo '<table class="leads-table">';
-                    echo '<thead>';
-                    echo '<tr>';
-                    echo '<th>ID</th>';
-                    echo '<th>Name</th>';
-                    echo '<th>Email</th>';
-                    echo '<th>Phone</th>';
-                    echo '<th>From City</th>';
-                    echo '<th>Destination City</th>';
-                    echo '<th>Departure Date</th>';
-                    echo '<th>Return Date</th>';
-                    echo '<th>Citizenship</th>';
-                    echo '<th>Additional Details</th>';
-                    echo '</tr>';
-                    echo '</thead>';
-                    echo '<tbody class="leads-table-body">';
-
-                    while ($row = $leadsResult->fetch_assoc()) {
-                        echo '<tr>';
-                        echo '<td>' . $row['id'] . '</td>';
-                        echo '<td>' . $row['name'] . '</td>';
-                        echo '<td>' . $row['email'] . '</td>';
-                        echo '<td>' . $row['phone'] . '</td>';
-                        echo '<td>' . $row['from_city'] . '</td>';
-                        echo '<td>' . $row['destination_city'] . '</td>';
-                        echo '<td>' . $row['departure_date'] . '</td>';
-                        echo '<td>' . $row['return_date'] . '</td>';
-                        echo '<td>' . $row['citizenship'] . '</td>';
-                        echo '<td>' . $row['additional_details'] . '</td>';
-                        echo '</tr>';
-                    }
-
-                    echo '</tbody>';
-                    echo '</table>';
-                    echo '</div>';
-                } else {
-                    echo '<div class="error-message">No leads found.</div>';
-                }
-            } else {
-                // Show login form if user is not logged in
-                echo '<h2>Login</h2>';
-                echo '<form method="post" action="">';
-                echo '<div class="form-group">';
-                echo '<label for="username">Username</label>';
-                echo '<input type="text" id="username" name="username" required>';
-                echo '</div>';
-                echo '<div class="form-group">';
-                echo '<label for="password">Password</label>';
-                echo '<input type="password" id="password" name="password" required>';
-                echo '</div>';
-                echo '<input type="submit" name="login" value="Login">';
-                echo '</form>';
-            }
-
-            $conn->close();
             ?>
-
-            <script>
-                function searchLeads() {
-                    var input = document.getElementById('search-input');
-                    var filter = input.value.toUpperCase();
-                    var table = document.getElementsByClassName('leads-table-body')[0];
-                    var rows = table.getElementsByTagName('tr');
-
-                    for (var i = 0; i < rows.length; i++) {
-                        var cells = rows[i].getElementsByTagName('td');
-                        var found = false;
-
-                        for (var j = 0; j < cells.length; j++) {
-                            var cell = cells[j];
-
-                            if (cell) {
-                                var textValue = cell.textContent || cell.innerText;
-
-                                if (textValue.toUpperCase().indexOf(filter) > -1) {
-                                    found = true;
-                                    break;
-                                }
-                            }
-                        }
-
-                        if (found) {
-                            rows[i].style.display = '';
-                        } else {
-                            rows[i].style.display = 'none';
-                        }
-                    }
-                }
-            </script>
+            <form method="post" action="">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" id="password" name="password" required>
+                </div>
+                <input type="submit" name="login" value="Login">
+            </form>
         </div>
     </div>
 </body>
