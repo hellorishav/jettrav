36b723d7c4fc5249389408a332f7127d313f6eda
rishav <79098022+hellorishav@users.noreply.github.com>
2023-07-09 13:45:05 -0500

    Update header.php

--- rishav/qa/header.php
+++ rishav/qa/header.php
@@ -4,52 +4,46 @@
     <title>Jettrav</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap">
     <style>
-        /* CSS styles for the header */
+        body {
+            margin: 0;
+            padding: 0;
+        }
 
         .header {
+            background-color: #2196f3;
+            color: #fff;
+            padding: 20px;
             display: flex;
             justify-content: space-between;
             align-items: center;
-            padding: 20px;
-            background-color: #fff;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            z-index: 9999;
         }
 
-        .logo {
-            font-weight: bold;
+        .header-logo {
             font-size: 24px;
+            font-weight: bold;
         }
 
         .header-links {
             display: flex;
-            align-items: center;
+            gap: 10px;
         }
 
         .header-links a {
-            margin-left: 20px;
+            color: #fff;
             text-decoration: none;
-            color: #333;
+            font-weight: bold;
         }
 
-        .header-links a:hover {
-            color: #1976d2;
+        .container {
+            margin-top: 120px; /* Add top margin to the container to avoid overlapping with the header */
         }
 
-        /* Additional styles */
-
-        body {
-            margin-top: 60px; /* Add top margin to the body to avoid overlapping with the header */
-        }
+        /* Additional CSS styles, if needed */
     </style>
 </head>
 <body>
     <div class="header">
-        <div class="logo">Jettrav</div>
+        <div class="header-logo">Jettrav</div>
         <div class="header-links">
             <a href="login.php">Login</a>
             <a href="survey.php">Survey</a>
