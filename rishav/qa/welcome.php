3efa7cec5c99b105345850ac8c36479ad0265614
rishav <79098022+hellorishav@users.noreply.github.com>
2023-07-09 14:12:45 -0500

    Update welcome.php

--- rishav/qa/welcome.php
+++ rishav/qa/welcome.php
@@ -4,6 +4,31 @@ session_start();
 // Check if the 'username' session variable is set
 if (isset($_SESSION['username'])) {
     $username = $_SESSION['username'];
+
+    // Database credentials
+    $servername = "localhost";
+    $db_username = "u947421468_jettrav";
+    $db_password = "Jettrav@capstone1";
+    $dbname = "u947421468_jettrav";
+
+    // Create connection
+    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Fetch the user's name from the database
+    $query = "SELECT name FROM credentials WHERE username = '$username'";
+    $result = $conn->query($query);
+
+    if ($result && $result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $name = $row['name'];
+    } else {
+        $name = "User";
+    }
 } else {
     // Redirect to the login page if the user is not logged in
     header("Location: login.php");
@@ -17,6 +42,7 @@ if (isset($_SESSION['username'])) {
 <head>
     <title>Welcome to Jettrav</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <style>
         body {
             display: flex;
@@ -62,7 +88,7 @@ if (isset($_SESSION['username'])) {
     <div class="container">
         <div class="welcome-message">
             <i class="material-icons">check_circle</i>
-            <span>Welcome, <?php echo $username; ?>!</span>
+            <span>Welcome, <?php echo $name; ?>!</span>
         </div>
     </div>
 </body>
