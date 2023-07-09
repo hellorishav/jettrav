35fea1702cd3830c20fd53afdf383d519fd9d38e
rishav <79098022+hellorishav@users.noreply.github.com>
2023-07-08 21:31:45 -0500

    Update login.php

--- rishav/qa/login.php
+++ rishav/qa/login.php
@@ -1,4 +1,71 @@
-<?php session_start(); ?>
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "u947421468_jettrav";
+$password = "Jettrav@capstone1";
+$dbname = "u947421468_jettrav";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process form submission for account creation
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create-account'])) {
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Check if the username already exists
+    $checkQuery = "SELECT * FROM credentials WHERE username = '$username'";
+    $checkResult = $conn->query($checkQuery);
+
+    if ($checkResult && $checkResult->num_rows > 0) {
+        echo '<div class="error-message">Username already exists. Please choose a different username.</div>';
+    } else {
+        // Hash the password
+        $hashedPassword = hash('sha256', $password);
+
+        // Insert the new account into the database
+        $insertQuery = "INSERT INTO credentials (name, username, password, role) VALUES ('$name', '$username', '$hashedPassword', 'user')";
+        if ($conn->query($insertQuery) === TRUE) {
+            echo '<div class="success-message">Account created successfully.</div>';
+        } else {
+            echo '<div class="error-message">Error creating account: ' . $conn->error . '</div>';
+        }
+    }
+}
+
+// Process form submission for login
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Hash the entered password for comparison
+    $hashedPassword = hash('sha256', $password);
+
+    // Check if the username and hashed password match in the database
+    $loginQuery = "SELECT * FROM credentials WHERE username = '$username' AND password = '$hashedPassword'";
+    $loginResult = $conn->query($loginQuery);
+
+    if ($loginResult && $loginResult->num_rows > 0) {
+        echo '<div class="success-message">Login successful. Redirecting to Dashboard...</div>';
+
+        // Set the session variable from the "username" field
+        $_SESSION['username'] = $username;
+
+        // Redirect to dashboard.php
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        echo '<div class="error-message">Invalid username or password.</div>';
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,162 +73,10 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            font-family: 'Roboto', sans-serif;
-            background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-        }
-
-        .container {
-            max-width: 400px;
-            width: 100%;
-            padding: 40px;
-            background-color: #fff;
-            border-radius: 4px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-            text-align: center;
-            overflow-y: auto;
-            max-height: 80vh; /* Adjust the max-height as needed */
-        }
-
-        h1 {
-            color: #333;
-            margin-bottom: 40px;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 12px;
-            margin-bottom: 20px;
-            border: none;
-            border-radius: 4px;
-            background-color: #f5f5f5;
-            font-size: 16px;
-            transition: background-color 0.3s;
-            box-sizing: border-box;
-        }
-
-        input[type="text"]:focus,
-        input[type="password"]:focus {
-            background-color: #e0e0e0;
-        }
-
-        input[type="submit"] {
-            background-color: #2196f3;
-            color: #fff;
-            padding: 12px 20px;
-            border: none;
-            border-radius: 4px;
-            font-size: 16px;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #1976d2;
-        }
-
-        .form-group {
-            margin-bottom: 24px;
-            text-align: left;
-        }
-
-        label {
-            display: block;
-            color: #888;
-            margin-bottom: 8px;
-            font-size: 14px;
-        }
-
-        .material-icons {
-            vertical-align: middle;
-        }
-
-        .tab-links {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .tab-link {
-            flex-grow: 1;
-            padding: 10px;
-            background-color: #eee;
-            border-radius: 4px 4px 0 0;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-
-        .tab-link:hover {
-            background-color: #ddd;
-        }
-
-        .tab-link.active {
-            background-color: #fff;
-        }
-
-        .tab-content {
-            display: none;
-            padding: 20px;
-            background-color: #fff;
-            border-radius: 0 4px 4px 4px;
-        }
-
-        .tab-content.active {
-            display: block;
-        }
-
-        .error-message {
-            color: #ff0000;
-            margin-bottom: 10px;
-        }
-
-        .success-message {
-            color: #009900;
-            margin-bottom: 10px;
-        }
+        /* CSS styles omitted for brevity */
     </style>
     <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            // Set the login tab as active by default
-            document.querySelector('.tab-link[data-tab="login-tab"]').classList.add('active');
-            document.getElementById('login-tab').classList.add('active');
-
-            // Add event listener to tab links for tab switching
-            var tabLinks = document.querySelectorAll('.tab-link');
-            tabLinks.forEach(function(link) {
-                link.addEventListener('click', function() {
-                    var tabName = this.dataset.tab;
-                    switchTab(tabName);
-                });
-            });
-        });
-
-        function switchTab(tabName) {
-            var tabLinks = document.querySelectorAll('.tab-link');
-            var tabContents = document.querySelectorAll('.tab-content');
-
-            // Remove active class from all tab links and contents
-            tabLinks.forEach(function(link) {
-                link.classList.remove('active');
-            });
-
-            tabContents.forEach(function(content) {
-                content.classList.remove('active');
-            });
-
-            // Add active class to the clicked tab link and corresponding content
-            document.querySelector('.tab-link[data-tab="' + tabName + '"]').classList.add('active');
-            document.getElementById(tabName).classList.add('active');
-        }
+        // JavaScript code omitted for brevity
     </script>
 </head>
 <body>
@@ -175,45 +90,6 @@
 
         <div id="create-account-tab" class="tab-content">
             <h2>Create Account</h2>
-            <?php
-            $servername = "localhost";
-            $username = "u947421468_jettrav";
-            $password = "Jettrav@capstone1";
-            $dbname = "u947421468_jettrav";
-
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create-account'])) {
-                $name = $_POST['name'];
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-
-                // Check if the username already exists
-                $checkQuery = "SELECT * FROM credentials WHERE username = '$username'";
-                $checkResult = $conn->query($checkQuery);
-
-                if ($checkResult && $checkResult->num_rows > 0) {
-                    echo '<div class="error-message">Username already exists. Please choose a different username.</div>';
-                } else {
-                    // Hash the password
-                    $hashedPassword = hash('sha256', $password);
-
-                    // Insert the new account into the database
-                    $insertQuery = "INSERT INTO credentials (name, username, password, role) VALUES ('$name', '$username', '$hashedPassword', 'user')";
-                    if ($conn->query($insertQuery) === TRUE) {
-                        echo '<div class="success-message">Account created successfully.</div>';
-                    } else {
-                        echo '<div class="error-message">Error creating account: ' . $conn->error . '</div>';
-                    }
-                }
-            }
-            ?>
             <form method="post" action="">
                 <div class="form-group">
                     <label for="name">Name</label>
@@ -233,32 +109,6 @@
 
         <div id="login-tab" class="tab-content">
             <h2>Login</h2>
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-
-                // Hash the entered password for comparison
-                $hashedPassword = hash('sha256', $password);
-
-                // Check if the username and hashed password match in the database
-                $loginQuery = "SELECT * FROM credentials WHERE username = '$username' AND password = '$hashedPassword'";
-                $loginResult = $conn->query($loginQuery);
-
-                if ($loginResult && $loginResult->num_rows > 0) {
-                    echo '<div class="success-message">Login successful. Redirecting to Dashboard...</div>';
-
-                    // Set the session variable from the "username" field
-                    $_SESSION['username'] = $username;
-
-                    // Redirect to dashboard.php
-                    //header("Location: dashboard.php");
-                    exit();
-                } else {
-                    echo '<div class="error-message">Invalid username or password.</div>';
-                }
-            }
-            ?>
             <form method="post" action="">
                 <div class="form-group">
                     <label for="username">Username</label>
