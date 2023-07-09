e35ef01e916a95dda3ee412c1c6718ebeb01d492
rishav <79098022+hellorishav@users.noreply.github.com>
2023-07-09 13:40:20 -0500

    Update header.php

--- rishav/qa/header.php
+++ rishav/qa/header.php
@@ -1,69 +1,10 @@
+<!DOCTYPE html>
+<html>
 <head>
+    <title>Jettrav</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap">
     <style>
-        body {
-            font-family: 'Roboto', sans-serif;
-            background-color: #f5f5f5;
-            margin: 0;
-            padding: 0;
-        }
-
-        .container {
-            width: 400px;
-            padding: 40px;
-            margin: 50px auto;
-            background-color: #fff;
-            border-radius: 4px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        }
-
-        h1 {
-            color: #333;
-            margin-bottom: 20px;
-        }
-
-        input[type="text"] {
-            width: 100%;
-            padding: 12px;
-            margin-bottom: 20px;
-            border: 1px solid #ddd;
-            border-radius: 4px;
-            background-color: #f5f5f5;
-            font-size: 16px;
-            box-sizing: border-box;
-        }
-
-        input[type="submit"] {
-            background-color: #2196f3;
-            color: #fff;
-            padding: 12px 20px;
-            border: none;
-            border-radius: 4px;
-            font-size: 16px;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #1976d2;
-        }
-
-        .itinerary-container {
-            margin-top: 20px;
-        }
-
-        .itinerary-item {
-            padding: 10px;
-            border: 1px solid #ddd;
-            border-radius: 4px;
-            background-color: #f5f5f5;
-            margin-bottom: 10px;
-        }
-
-        .error-message {
-            color: #ff0000;
-            margin-bottom: 10px;
-        }
+        /* CSS styles for the header */
 
         .header {
             display: flex;
@@ -79,6 +20,11 @@
             font-size: 24px;
         }
 
+        .header-links {
+            display: flex;
+            align-items: center;
+        }
+
         .header-links a {
             margin-left: 20px;
             text-decoration: none;
@@ -90,6 +36,7 @@
         }
     </style>
 </head>
+<body>
     <div class="header">
         <div class="logo">Jettrav</div>
         <div class="header-links">
@@ -99,3 +46,5 @@
             <a href="itinerary.php">Create Itinerary</a>
         </div>
     </div>
+</body>
+</html>
